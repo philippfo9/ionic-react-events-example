445d44ae38bca2404f66e3d5dfd8559ccbf4e7bd
philippfo9 <philipp@websteps.at>
2020-07-11 10:33:16 +0200

    adding card for map event

---
+++ src/components/MapEventCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MapEventCard.css';
+import { IonCol } from '@ionic/react';
+import EventCard from './EventCard';
+import { IEvent } from '../service/events';
+
+const MapEventCard: React.FC<{ event: IEvent }> = ({ event }) => {
+    return (
+        <IonCol size="12" sizeMd="4">
+            <EventCard event={event}></EventCard>
+        </IonCol>
+    );
+};
+
+export default MapEventCard;

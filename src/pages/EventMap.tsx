0dd0c535817c7d5c02d89192657be244da42a502
philippfo9 <philipp@websteps.at>
2020-07-11 10:34:27 +0200

    adding event map page

---
+++ src/pages/EventMap.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './EventMap.css';
+import MapWithEvents from '../components/MapWithEvents';
+import events, { IEvent } from '../service/events';
+import MapEventCard from '../components/MapEventCard';
+
+const EventMapPage: React.FC = () => {
+    const [eventForDetailView, setEventForDetailView] = useState<IEvent>();
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Suche nach Events auf der Map</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Events Map</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <div style={{ height: 'calc(100% - 51px)', width: '100%' }}>
+                    <MapWithEvents
+                        openEventCard={(event) => {
+                            setEventForDetailView(event);
+                        }}
+                        center={{ lat: 48.210033, lng: 16.363449 }}
+                        events={events}
+                        zoom={11}
+                    ></MapWithEvents>
+                </div>
+                {eventForDetailView && (
+                    <div
+                        style={{
+                            position: 'absolute',
+                            left: '0',
+                            width: '100vw',
+                            bottom: '0',
+                            display: 'flex',
+                            alignContent: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <MapEventCard event={eventForDetailView}></MapEventCard>
+                    </div>
+                )}
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default EventMapPage;

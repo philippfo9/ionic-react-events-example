59ac61637849214539cc85cfaf6f51551d351656
philippfo9 <philipp@websteps.at>
2020-07-11 10:35:17 +0200

    modifying App.tsx to use right routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
@@ -11,9 +11,9 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import UserHome from './pages/UserHome';
+import EventList from './pages/EventList';
+import EventMap from './pages/EventMap';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,34 +33,43 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import UserContext from './service/userContext';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [user, setUser] = useState({
+    name: 'Gustav',
+    email: 'gustavo@diblo.at'
+  });
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <UserContext.Provider value={user}>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route path="/profile" component={UserHome} exact={true} />
+              <Route path="/events-list" component={EventList} exact={true} />
+              <Route path="/events-map" component={EventMap} />
+              <Route path="/" render={() => <Redirect to="/profile" />} exact={true} />
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="profile" href="/profile">
+                <IonIcon icon={triangle} />
+                <IonLabel>Profil</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="eventList" href="/events-list">
+                <IonIcon icon={ellipse} />
+                <IonLabel>Eventliste</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="eventsMap" href="/events-map">
+                <IonIcon icon={square} />
+                <IonLabel>Map</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </UserContext.Provider>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
